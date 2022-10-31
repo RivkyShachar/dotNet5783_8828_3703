d996661999610a1b75108cf178bada16f5b76482
David <David@DESKTOP-J2PMT7Q>
2022-10-31 16:18:49 +0200

    welcome functions

---
+++ dotNet5783_8828-3703/Stage0/Program8828.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8828();
+            Console.ReadKey();
+        }
+        static partial void Welcome3703();
+        private static void Welcome8828()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
