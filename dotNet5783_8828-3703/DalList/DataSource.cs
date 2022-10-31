18feafd2ccd5666f00cf758c8da217265e0dae9b
David <David@DESKTOP-J2PMT7Q>
2022-10-31 20:12:51 +0200

    did till stage 1 page 7 second line

--- dotNet5783_8828-3703/DalList/DataSource.cs
+++ dotNet5783_8828-3703/DalList/DataSource.cs
@@ -4,7 +4,40 @@ namespace Dal;
 
 internal static class DataSource
 {
-    internal static List<Order> orders;
-    internal static List<OrderItem> orderItems;
-    internal static List<Product> products;
+    //need to fix capital letters and such
+
+    //need to check in its really public
+    //need to check if this is the right way to write this part(random)
+    public static Random? random = new Random();
+    public static int? randNum = random.Next(1,20);
+
+    internal static List<Order>? orders;
+    internal static List<OrderItem>? orderItems;
+    internal static List<Product>? products;
+
+    //static DataSource()
+    //{
+    //    s_Initialize()
+    //}
+    private static List<Order> AddOrder()
+    {
+        orders.Add(new Order());
+        return orders;
+    }
+    private static List<OrderItem> AddOrderItem()
+    {
+        orderItems.Add(new OrderItem());
+        return orderItems;
+    }
+    private static List<Product> AddProduct()
+    {
+        products.Add(new Product());
+        return products;
+    }
+    //private void static s_Initialize()
+    //{
+    //    AddProduct();
+    //    AddOrder();
+    //    AddOrderItem();
+    //}
 }
