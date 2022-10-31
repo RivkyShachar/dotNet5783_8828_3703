2b216f41bb95b37b9571acbd17e21d235348473a
Sarah koskas <Sarahkoskas2004@gmail.com>
2022-10-31 20:12:12 +0200

    Ishuyot Netunim: structures

--- dotNet5783_8828-3703/DalFacade/DO/OrderItem.cs
+++ dotNet5783_8828-3703/DalFacade/DO/OrderItem.cs
@@ -1,5 +1,17 @@
 ﻿namespace DO;
 
 public struct OrderItem
-{
+{/// <summary>
+/// Structure for Order Item, contains Product Id, Order Id, Price, Amount + ToString override
+/// </summary>
+    public int ProductID { get; set; }
+    public int OrderID { get; set; }
+    public double Price { get; set; }
+    public int Amount { get; set; }
+    public override string ToString() => $@"
+    Product ID= {ProductID}
+    Order ID = {OrderID}
+    Price - {Price}
+    Amount - {Amount}
+   ";
 }
--- dotNet5783_8828-3703/DalFacade/DO/Product.cs
+++ dotNet5783_8828-3703/DalFacade/DO/Product.cs
@@ -1,5 +1,21 @@
-﻿namespace DO;
+﻿using System.ComponentModel;
+
+namespace DO;
 
 public struct Product
-{
+{/// <summary>
+/// Structure for Products, contains Id, Name, Price, Category, InStock + ToString override
+/// </summary>
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public CategoryAttribute Category { get; set; }
+    public int InStock { get; set; }
+    public override string ToString() => $@"
+        Product ID={ID}: {Name} 
+        category : {Category}
+    	Price: {Price}
+    	Amount in stock: {InStock}
+";
+
 }
