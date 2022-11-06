2852415add89b7b614a714f12159eeaaefb61c87
David <David@DESKTOP-J2PMT7Q>
2022-11-06 20:41:07 +0200

    product meniu start

--- dotNet5783_8828-3703/DalList/DalOrderItem.cs
+++ dotNet5783_8828-3703/DalList/DalOrderItem.cs
@@ -4,5 +4,39 @@ namespace Dal;
 
 public class DalOrderItem
 {
+    #region OrderItem Function
+    public void AddOrderItem(OrderItem orderItem)
+    {
+        DataSource.orderItemsList.Add(orderItem);
+        //need to ask what it returns
+        //return orderItem.OrderID;
+    }
+    public bool DeleteOrderItem(int id)
+    {
+        OrderItem ordItm = GetOrderItem(id);
+        if (ordItm.Equals(null))
+            throw new Exception("Order item with the same id not found...");
+        DataSource.orderItemsList.RemoveAll(sc => sc.OrderID == id);
+        return DataSource.orderItemsList.Remove(ordItm);
+    }
+    public void UpdateOrderItem(OrderItem orderItem)
+    {
+        int index = DataSource.orderItemsList.FindIndex(s => s.OrderID == orderItem.OrderID);
+        if (index == -1)
+            throw new Exception("Order item with the same id not found...");
+        DataSource.orderItemsList[index] = orderItem;
+    }
+    public OrderItem GetOrderItem(int id)
+    {
+        //need to ask what it returns
+        return DataSource.orderItemsList.FirstOrDefault(s => s.OrderID == id);
+    }
+    public IEnumerable<OrderItem> GetAllOrederItems(Func<OrderItem, bool> predicat = null)
+    {
+        if (predicat == null)
+            return DataSource.orderItemsList.AsEnumerable();
 
+        return DataSource.orderItemsList.Where(predicat);
+    }
+    #endregion
 }
